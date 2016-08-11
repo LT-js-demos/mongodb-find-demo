96decc74dde7500b75e28eb293f816b9c2b06dac
Leonard LiuTao <364125168@qq.com>
2016-08-11 09:49:39 +0800

    move the connect from findOne() to outside

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,23 @@ var User = mongoose.model('User', {
     password: String
 });
 
+mongoose.connect("mongodb://localhost/mongodb-find-demo-db");
+
 exports.insert = function (request, response) {
     var user = new User({
         name: 'Leonard',
-        password: 'admin'
+        password: '123'
     });
-    mongoose.connect("mongodb://localhost/mongodb-find-demo-db");
     user.save(function (err) {
         console.log('save status:', err ? 'failed' : 'success');
     });
 };
 
 exports.find = function (request, response) {
-    mongoose.connect("mongodb://localhost/mongodb-find-demo-db");
     User.findOne({name:"Leonard"},function (error, docs) {
         if(error) response.send(error.message);
-        response.send("username:"+docs.name+",password:"+docs.password);
+        response.send("username:"+docs.name+",password:"+docs.password+`
+login success`);
     });
 
 };
